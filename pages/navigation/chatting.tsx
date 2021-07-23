310c77d85756d2622f883b6b53a2cc9cea818377
blissful-y0 <blissful.y0@gmail.com>
2021-07-23 15:26:33 +0900

    update: Chat feature onging

--- pages/navigation/chatting.tsx
+++ pages/navigation/chatting.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import ChattingPage from '../screens/chatting'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import ChattingPage from "../screens/chatting";
 
 const ChattingNavigator = () => {
-  const Stact = createStackNavigator()
+  const Stact = createStackNavigator();
 
   return (
-    <Stact.Navigator screenOptions={{headerShown: false}}>
-      <Stact.Screen name='chatting' component={ChattingPage}></Stact.Screen>
+    <Stact.Navigator screenOptions={{ headerShown: false }}>
+      <Stact.Screen name="chatting" component={ChattingPage}></Stact.Screen>
     </Stact.Navigator>
-  )
-}
+  );
+};
 
-export default ChattingNavigator
+export default ChattingNavigator;
